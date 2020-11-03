a061b271c79a2b735831ae986507bfeae5c2981b
Konstantine Karantasis <konstantine@confluent.io>
2020-11-03 13:28:47 -0800

    Reshade transitive dependency htrace-core4 with newer jackson to remove CVEs (#158)
    * Reshade transitive dependency htrace-core4 with newer jackson to remove CVEs
    
    * Add new module in the right order
    
    * Keep META-INF files from htrace not jackson
    
    * Add maven-jar-plugin explicitly
    
    * Add packaging type
    
    * Add a place holder class that is removed during shading to bypass issues with an empty produced jar

---
+++ htrace-core4-shaded/src/main/java/io/confluent/connect/storage/PlaceHolder.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.connect.storage;
+
+/**
+ * This class is added as a workaround to maven. Attempting to run both verify and install phases
+ * with maven fails if there's no source code in the project (the produced jar is empty).
+ * {@see http://mail-archives.apache.org/mod_mbox/maven-users/201608.mbox/%3c20160805151905.500d5c45@copperhead.int.arc7.info%3e}
+ * <br>
+ * This fact subsequently fails the attempt to re-shade the dependencies without adding new code.
+ * The workaround is to introduce this class here, with the intention to remove it during shading
+ * by configuring a filter for maven-shade-plugin.
+ */
+public class PlaceHolder {
+}
