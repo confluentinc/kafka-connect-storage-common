e862b3854cd42713db8a17c2e26f21668af90b1c
Daniel Osvath <dosvath@confluent.io>
2021-04-14 13:52:41 -0400

    Stylecheck

--- wal/src/main/java/io/confluent/connect/storage/wal/FilePathOffset.java
+++ wal/src/main/java/io/confluent/connect/storage/wal/FilePathOffset.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.connect.storage.wal;
 
 /**
@@ -27,9 +42,9 @@ public class FilePathOffset {
 
   @Override
   public String toString() {
-    return "FilePathOffset{" +
-        "extractedOffset=" + extractedOffset +
-        ", filePath='" + filePath + '\'' +
-        '}';
+    return "FilePathOffset{"
+        + "extractedOffset=" + extractedOffset
+        + ", filePath='" + filePath + '\''
+        + '}';
   }
 }
