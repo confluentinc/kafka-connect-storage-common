ff9e2cc9feecd60915ac0164a0a0eec77b1a7f9e
Sarthak Sonagara <ssonagara@confluent.io>
2026-01-23 11:03:29 +0530

    CC-37472: Adding FieldPartitionerValidator to validate empty/null partition field name (#462)
    * Adding validation to validate empty/null partition field name
    
    * Adding tests for FieldPartitonerValidator
    
    * Updated method signature of validate
    
    * Revert "Updated method signature of validate"
    
    This reverts commit fdf33a1bc1d12054c9335a9521592d49b71539de.
    
    * Updated validator to validate correctly for inheritors of FieldPartitioner
    
    * Remove Current thread's class loader
    
    * Simplified method to get the class

---
+++ partitioner/src/test/java/io/confluent/connect/storage/partitioner/FieldPartitionerValidatorTest.java
@@ -0,0 +1,118 @@
+package io.confluent.connect.storage.partitioner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import io.confluent.connect.storage.StorageSinkTestBase;
+import io.confluent.connect.storage.common.GenericRecommender;
+import java.util.List;
+import org.apache.kafka.common.config.Config;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigValue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FieldPartitionerValidatorTest extends StorageSinkTestBase {
+
+  private ConfigDef configDef;
+
+  @Before
+  public void setUp() {
+    properties = super.createProps();
+    configDef = PartitionerConfig.newConfigDef(new GenericRecommender());
+  }
+
+  @Test
+  public void testFieldPartitionerMissingFieldNameAddsError() {
+    properties.put(
+        PartitionerConfig.PARTITIONER_CLASS_CONFIG,
+        FieldPartitioner.class.getName()
+    );
+
+    Config config = new Config(configDef.validate(properties));
+    new FieldPartitionerValidator(properties, config).validate();
+
+    ConfigValue fieldNameValue = getConfigValue(config,
+        PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    List<String> errors = fieldNameValue.errorMessages();
+    assertEquals(1, errors.size());
+    assertEquals(
+        "Partition field name cannot be null or empty when using FieldPartitioner.",
+        errors.get(0)
+    );
+  }
+
+  @Test
+  public void testFieldPartitionerEmptyFieldNameAddsError() {
+    properties.put(
+        PartitionerConfig.PARTITIONER_CLASS_CONFIG,
+        FieldPartitioner.class.getName()
+    );
+    properties.put(PartitionerConfig.PARTITION_FIELD_NAME_CONFIG, "   ");
+
+    Config config = new Config(configDef.validate(properties));
+    new FieldPartitionerValidator(properties, config).validate();
+
+    ConfigValue fieldNameValue = getConfigValue(config,
+        PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    assertEquals(1, fieldNameValue.errorMessages().size());
+  }
+
+  @Test
+  public void testNonFieldPartitionerDoesNotAddError() {
+    properties.put(
+        PartitionerConfig.PARTITIONER_CLASS_CONFIG,
+        DefaultPartitioner.class.getName()
+    );
+
+    Config config = new Config(configDef.validate(properties));
+    new FieldPartitionerValidator(properties, config).validate();
+
+    ConfigValue fieldNameValue = getConfigValue(config,
+        PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    assertTrue(fieldNameValue.errorMessages().isEmpty());
+  }
+
+  @Test
+  public void testValidPartitionFieldNameDoesNotAddError() {
+    properties.put(
+        PartitionerConfig.PARTITIONER_CLASS_CONFIG,
+        FieldPartitioner.class.getName()
+    );
+    properties.put(PartitionerConfig.PARTITION_FIELD_NAME_CONFIG, "validFieldName");
+
+    Config config = new Config(configDef.validate(properties));
+    new FieldPartitionerValidator(properties, config).validate();
+
+    ConfigValue fieldNameValue = getConfigValue(config,
+        PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    assertTrue(fieldNameValue.errorMessages().isEmpty());
+  }
+
+  @Test
+  public void testSubclassOfFieldPartitionerAddsError() {
+    properties.put(
+        PartitionerConfig.PARTITIONER_CLASS_CONFIG,
+        CustomFieldPartitioner.class.getName()
+    );
+
+    Config config = new Config(configDef.validate(properties));
+    new FieldPartitionerValidator(properties, config).validate();
+
+    ConfigValue fieldNameValue = getConfigValue(config,
+        PartitionerConfig.PARTITION_FIELD_NAME_CONFIG);
+    assertEquals(1, fieldNameValue.errorMessages().size());
+  }
+
+  private static class CustomFieldPartitioner extends FieldPartitioner<Object> {
+  }
+
+  private ConfigValue getConfigValue(Config config, String name) {
+    for (ConfigValue configValue : config.configValues()) {
+      if (name.equals(configValue.name())) {
+        return configValue;
+      }
+    }
+    throw new AssertionError("Missing ConfigValue for " + name);
+  }
+}
